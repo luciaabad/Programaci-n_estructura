286469f68b05cb635256846ba037d77a284cccba
luciaabad <labadgut@myuax.com>
2024-11-22 12:35:52 +0100

    Estudiante 1 y uniones

--- main.c
+++ main.c
@@ -36,6 +36,25 @@ int main() {
     imprimirEstudiante(estudianteCopiado);
     printf("\n");
 
+    union Dato {
+        int i;
+        float f;
+        char str[20];
+    };
+
+    union Dato dato;
+    dato.i = 10;
+    printf("dato.i: %d\n", dato.i);
+
+    dato.f = 220.5;
+    printf("dato.f: %.2f\n", dato.f);
+
+    strcpy(dato.str, "C programming");
+    printf("dato.str: %s\n", dato.str);
+
+    free(estudiante2);
+
+
 
 
 
